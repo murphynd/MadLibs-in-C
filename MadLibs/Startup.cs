e0eef7caef62f314d8bbcd16f5f5ad168fe79411
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-28 13:23:59 -0700

    this adds wwwroot for css file to work with Views
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- MadLibs/Startup.cs
+++ MadLibs/Startup.cs
@@ -25,6 +25,7 @@ namespace MadLibs
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseStaticFiles();
       app.UseDeveloperExceptionPage();
       app.UseMvc(routes =>
       {
