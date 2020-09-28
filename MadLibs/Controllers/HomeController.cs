4c6e971e11a1a041f3b39c0c5674be4993fe8455
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-28 10:35:53 -0700

    controller is built for form logic and model built for form.
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,13 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
 
-    public string Hello() { return "Hello friend!"; }
+    [Route("/Story")]
+    public ActionResult Madlibs(string holiday, string noun1,
+    string place1, string person1, string adj1, string bodypart,
+    string verb1, string adj2, string noun2, string food,
+    string plNoun, string famPers, string noun3, string food2,
+    string food3, string number, string verb2, string familyMem,
+     string food4, string bodypart2)
+    {
+      MadLibsVariable myMadLibsVariable = new MadLibsVariable();
+      myMadLibsVariable.holiday = holiday;
+      myMadLibsVariable.noun1 = noun1;
+      myMadLibsVariable.place1 = place1;
+      myMadLibsVariable.person1 = person1;
+      myMadLibsVariable.adj1 = adj1;
+      myMadLibsVariable.bodypart = bodypart;
+      myMadLibsVariable.verb1 = verb1;
+      myMadLibsVariable.adj2 = adj2;
+      myMadLibsVariable.noun2 = noun2;
+      myMadLibsVariable.food = food;
+      myMadLibsVariable.plNoun = plNoun;
+      myMadLibsVariable.famPers = famPers;
+      myMadLibsVariable.noun3 = noun3;
+      myMadLibsVariable.food2 = food2;
+      myMadLibsVariable.food3 = food3;
+      myMadLibsVariable.number = number;
+      myMadLibsVariable.verb2 = verb2;
+      myMadLibsVariable.familyMem = familyMem;
+      myMadLibsVariable.food4 = food4;
+      myMadLibsVariable.bodypart2 = bodypart2;
 
-    public string Goodbye() { return "Goodbye friend."; }
+      return View(myMadLibsVariable);
+    }
 
   }
 }
--- MadLibs/Models/MadLibsVariable.cs
+++ MadLibs/Models/MadLibsVariable.cs
@@ -2,6 +2,26 @@ namespace MadLibs.Models
 {
   public class MadLibsVariable
   {
+    public string holiday { get; set; }
+    public string noun1 { get; set; }
+    public string place1 { get; set; }
+    public string person1 { get; set; }
+    public string adj1 { get; set; }
+    public string bodypart { get; set; }
+    public string verb1 { get; set; }
+    public string verb2 { get; set; }
+    public string adj2 { get; set; }
+    public string noun2 { get; set; }
+    public string food { get; set; }
+    public string plNoun { get; set; }
+    public string famPers { get; set; }
+    public string noun3 { get; set; }
+    public string food2 { get; set; }
+    public string food3 { get; set; }
+    public string number { get; set; }
+    public string familyMem { get; set; }
+    public string food4 { get; set; }
+    public string bodypart2 { get; set; }
 
   }
 }
